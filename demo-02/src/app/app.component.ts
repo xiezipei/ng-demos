b180ef698c25772770174dc01ee611d2824f3d39
xiezipei <zipeixie@gmail.com>
2022-08-26 10:29:38 +0800

    feat(demo01):  示例 1:数据渲染

--- demo-02/src/app/app.component.ts
+++ demo-02/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Hello World!';
+  // 示例 1:数据渲染
+  message = 'Hello Angular!';
 }
