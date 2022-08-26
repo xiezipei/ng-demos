cd7f5833fe74620957d493186ecab9619a683f09
xiezipei <zipeixie@gmail.com>
2022-08-26 10:41:40 +0800

    feat(demo02): 示例2-3

--- demo-02/src/app/app.component.ts
+++ demo-02/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+
+/**
+ * 物品
+ */
+export interface Item {
+  /** 序号 */
+  id: number;
+  /** 名称 */
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,6 +16,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // 示例 1:数据渲染
+  /** 示例 1:数据渲染 */
   message = 'Hello Angular!';
+
+  /** 示例 2:循环渲染列表 */
+  items: Item[] = [
+    { id: 1, name: 'Apple' },
+    { id: 2, name: 'Banana' },
+    { id: 3, name: 'Cat' },
+  ];
+
+  /** 示例 3:循条件判断与事件绑定 */
+  isShow = false;
+  version = VERSION;
+  showVersion() {
+    console.log('version', this.version);
+    this.isShow = true;
+  }
 }
