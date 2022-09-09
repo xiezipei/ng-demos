1dd73e6649bf35533657512566bed6d66ebd5dbd
xiezipei <zipeixie@gmail.com>
2022-09-09 15:16:45 +0800

    feat: update pass-by-localvar

--- demo-11-components/src/app/components/component-interaction/pass-by-localvar/pass-by-localvar.component.ts
+++ demo-11-components/src/app/components/component-interaction/pass-by-localvar/pass-by-localvar.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pass-by-localvar',
   templateUrl: './pass-by-localvar.component.html',
-  styleUrls: ['./pass-by-localvar.component.scss']
+  styleUrls: ['./pass-by-localvar.component.scss'],
 })
-export class PassByLocalvarComponent implements OnInit {
+export class PassByLocalvarComponent implements OnInit, OnDestroy {
+  intervalId = 0;
+  message = '';
+  seconds = 11;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.clearTimer();
   }
 
+  start() {
+    this.countDown();
+  }
+
+  stop() {
+    this.clearTimer();
+    this.message = `Holding at T-${this.seconds} seconds`;
+  }
+
+  private clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.seconds -= 1;
+      if (this.seconds === 0) {
+        this.message = 'Blast off!';
+      } else {
+        if (this.seconds < 0) {
+          this.seconds = 10;
+        } // reset
+        this.message = `T-${this.seconds} seconds and counting`;
+      }
+    }, 1000);
+  }
 }
