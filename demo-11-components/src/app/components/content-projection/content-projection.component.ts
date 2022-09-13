194044d8dcf36abc79303b46cf577d4142de39d2
xiezipei <zipeixie@gmail.com>
2022-09-13 15:55:29 +0800

    feat: content-projection

--- demo-11-components/src/app/components/content-projection/content-projection.component.ts
+++ demo-11-components/src/app/components/content-projection/content-projection.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content-projection',
   templateUrl: './content-projection.component.html',
-  styleUrls: ['./content-projection.component.scss']
+  styleUrls: ['./content-projection.component.scss'],
 })
 export class ContentProjectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
